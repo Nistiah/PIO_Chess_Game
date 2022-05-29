d63b3af18951dce178285aa0f86f0544214b89b8
MateuszAdamski <235823@edu.p.lodz.pl>
2022-05-29 19:42:00 +0200

    2. Stworzenie szkieletu klasy board + stworzenie planszy + małe poprawki PieceSet

--- src/main/java/piochess/pio_chess/PieceSet.java
+++ src/main/java/piochess/pio_chess/PieceSet.java
@@ -24,39 +24,39 @@ public class PieceSet {
         this.color = color;
 
         if(color == 0) {
-            Piece pawn1 = new Pawn(0,2);
-            Piece pawn2 = new Pawn(1,2);
-            Piece pawn3 = new Pawn(2,2);
-            Piece pawn4 = new Pawn(3,2);
-            Piece pawn5 = new Pawn(4,2);
-            Piece pawn6 = new Pawn(5,2);
-            Piece pawn7 = new Pawn(6,2);
-            Piece pawn8 = new Pawn(7,2);
-            Piece rook1 = new Rook(8,2);
-            Piece knight1 = new Knight(0,1);
-            Piece bishop1 = new Bishop(1,1);
-            Piece queen = new Queen(2,1);
-            Piece king = new King(2,3);
-            Piece bishop2 = new Bishop(2,4);
-            Piece knight2 = new Knight(2,5);
-            Piece rook2 = new Rook(2,6);
+            pawn1 = new Pawn(0,2);
+            pawn2 = new Pawn(1,2);
+            pawn3 = new Pawn(2,2);
+            pawn4 = new Pawn(3,2);
+            pawn5 = new Pawn(4,2);
+            pawn6 = new Pawn(5,2);
+            pawn7 = new Pawn(6,2);
+            pawn8 = new Pawn(7,2);
+            rook1 = new Rook(0,1);
+            knight1 = new Knight(1,1);
+            bishop1 = new Bishop(2,1);
+            queen = new Queen(3,1);
+            king = new King(4,1);
+            bishop2 = new Bishop(5,1);
+            knight2 = new Knight(6,1);
+            rook2 = new Rook(7,1);
         }else{
-            Piece pawn1 = new Pawn(0,7);
-            Piece pawn2 = new Pawn(1,7);
-            Piece pawn3 = new Pawn(2,7);
-            Piece pawn4 = new Pawn(3,7);
-            Piece pawn5 = new Pawn(4,8);
-            Piece pawn6 = new Pawn(5,7);
-            Piece pawn7 = new Pawn(6,7);
-            Piece pawn8 = new Pawn(7,7);
-            Piece rook1 = new Rook(0,8);
-            Piece knight1 = new Knight(1,8);
-            Piece bishop1 = new Bishop(2,8);
-            Piece king = new King(3,8);
-            Piece queen = new Queen(4,8);
-            Piece bishop2 = new Bishop(5,8);
-            Piece knight2 = new Knight(6,8);
-            Piece rook2 = new Rook(7,8);
+            pawn1 = new Pawn(0,7);
+            pawn2 = new Pawn(1,7);
+            pawn3 = new Pawn(2,7);
+            pawn4 = new Pawn(3,7);
+            pawn5 = new Pawn(4,7);
+            pawn6 = new Pawn(5,7);
+            pawn7 = new Pawn(6,7);
+            pawn8 = new Pawn(7,7);
+            rook1 = new Rook(0,8);
+            knight1 = new Knight(1,8);
+            bishop1 = new Bishop(2,8);
+            king = new King(3,8);
+            queen = new Queen(4,8);
+            bishop2 = new Bishop(5,8);
+            knight2 = new Knight(6,8);
+            rook2 = new Rook(7,8);
         }
 
 
