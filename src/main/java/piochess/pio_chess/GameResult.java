cdbe7f18e91442468b31b7a53ce7ccc0f4a045f5
MateuszAdamski <235823@edu.p.lodz.pl>
2022-06-06 01:54:44 +0200

    3. klasa gameResult

--- src/main/java/piochess/pio_chess/GameResult.java
+++ src/main/java/piochess/pio_chess/GameResult.java
@@ -1,4 +1,13 @@
 package piochess.pio_chess;
 
 public class GameResult {
+    Player player1;
+    Player player2;
+
+
+    public GameResult(Player p1, Player p2)
+    {
+        player1 = p1;
+        player2 = p2;
+    }
 }
